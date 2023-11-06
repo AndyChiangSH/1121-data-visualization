64d0c9ee9ffce48af0275b1085c6be204478c12c
AndyChiangSH <andy10801@gmail.com>
2023-11-07 00:18:37 +0800

    HW7: done

---
+++ Homeworks/HW7_Horizon Chart/312553024.js
@@ -0,0 +1,70 @@
+// const data_path = "http://vis.lab.djosix.com:2023/data/air-pollution.csv"
+const data_path = "./air-pollution.csv"
+
+d3.csv(data_path).then(function (data) { 
+    // console.log("data:", data)
+
+    roundTo = function (num, decimal) { return Math.round((num + Number.EPSILON) * Math.pow(10, decimal)) / Math.pow(10, decimal); }
+
+    function aggregate(data, type) {
+        var sums = data.reduce(function (acc, obj) {
+            var date = obj["Measurement date"].split(" ")[0];
+            var station = obj["Station code"];
+            if (!acc[date]) {
+                acc[date] = {};
+            }
+            if (!acc[date][station]) {
+                acc[date][station] = { sum: 0, count: 0 };
+            }
+            acc[date][station].sum += +obj[type];
+            acc[date][station].count++;
+            return acc;
+        }, Object.create(null));
+
+        return Object.keys(sums).map(function (date) {
+            // console.log("sums:", sums)
+            // console.log("sums[date]:", sums[date])
+            return Object.keys(sums[date]).map(function (station) {
+                // console.log("date:", date)
+                // console.log("station:", station)
+                return {
+                    "ts": new Date(date),
+                    "series": station,
+                    "val": roundTo(sums[date][station].sum / sums[date][station].count, 4),
+                };
+            });
+        });
+    }
+
+    // add an event listener for the change event
+    const radioButtons = document.querySelectorAll('input[name="type"]');
+    for (const radioButton of radioButtons) {
+        radioButton.addEventListener('change', showSelected);
+    }
+
+    function showSelected(e) {
+        // console.log(e);
+        if (this.checked) {
+            render(this.value)
+        }
+    }
+    
+    function render(type) {
+        var data_2 = aggregate(data, type)
+        // console.log("data_2:", data_2)
+
+        var data_3 = [].concat(...data_2);
+        // console.log("data_3:", data_3)
+
+        HorizonTSChart()(document.getElementById('horizon-chart'))
+            .data(data_3)
+            .series('series')
+        // .yAggregation([fn([numbers])])
+        // .yNormalize(true);
+    }
+
+    render("SO2");
+});
+
+
+
